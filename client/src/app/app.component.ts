7d4be67c53963657b92bfa66ea56d43adbb43c16
daitro123 <macek.tom@gmail.com>
2022-08-12 19:14:24 +0200

    front-end created

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (users) => (this.users = users),
+      error: (err) => console.log(err),
+    });
+  }
+}
